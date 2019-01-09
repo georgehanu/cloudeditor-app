3c19d32611ccf77854accd54be74bdc302f43ea2
Miki <ig@cloudlab.at>
2019-01-09 18:14:56 +0200

    Invalid - change in selection.js to work local

--- cloudeditor/core/stores/reducers/selection.js
+++ cloudeditor/core/stores/reducers/selection.js
@@ -3,5 +3,6 @@ const ProjectUtils = require("../../utils/ProjectUtils");
 const ConfigUtils = require("../../utils/ConfigUtils");
 const config = ConfigUtils.getDefaults();
 
-const initialState = ProjectUtils.getEmptySelection(config.selection);
+/* To work local */
+const initialState = {}; //ProjectUtils.getEmptySelection(config.selection);
 module.exports = handleActions({}, initialState);
