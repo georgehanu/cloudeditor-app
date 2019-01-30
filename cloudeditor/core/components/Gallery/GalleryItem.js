885c8f999670684c26ed2fd967c4795cda7cbfc4
Miki <ig@cloudlab.at>
2019-01-30 10:05:35 +0200

    DragPreview - DragLayer for images

--- cloudeditor/core/components/Gallery/GalleryItem.js
+++ cloudeditor/core/components/Gallery/GalleryItem.js
@@ -4,6 +4,10 @@ const { hot } = require("react-hot-loader");
 const { DragSource } = require("react-dnd");
 const uuidv4 = require("uuid/v4");
 const type = "image";
+
+const GalleryDragLayer = require("./GalleryDragLayer");
+const { getEmptyImage } = require("react-dnd-html5-backend");
+
 const PageSource = {
   beginDrag(props) {
     return {
@@ -24,35 +28,55 @@ const PageSource = {
 collectDrag = (connect, monitor) => {
   return {
     connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
     isDragging: monitor.isDragging()
   };
 };
-const galleryItem = props => {
-  const className =
-    "uploadGalleryItem " +
-    (props.selectedId === props.id ? "uploadGalleryItemSelected" : "");
+class galleryItem extends React.Component {
+  componentDidMount() {
+    // Use empty image as a drag preview so browsers don't draw it
+    // and we can draw whatever we want on the custom drag layer instead.
+    if (this.props.connectDragPreview) {
+      this.props.connectDragPreview(getEmptyImage(), {
+        // IE fallback: specify that we'd rather screenshot the node
+        // when it already knows it's being dragged so we can hide it with CSS.
+        captureDraggingState: true
+      });
+    }
+  }
 
-  return props.connectDragSource(
-    <div className={className}>
-      <img
-        src={props.thumbnail_src}
-        alt="galleryItem"
-        className="uploadGalleryItemImage"
-        /*onClick={() => props.selectImage(props.id)}*/
-      />
-      <div className="galleryItemActions">
-        <span
-          className="select icon printqicon-ok"
-          /*onClick={() => props.selectImage(props.id)}*/
+  render() {
+    const className =
+      "uploadGalleryItem " +
+      (this.props.selectedId === this.props.id
+        ? "uploadGalleryItemSelected"
+        : "");
+
+    return (
+      <div className={className}>
+        {this.props.connectDragPreview(
+          <div>
+            <GalleryDragLayer />
+          </div>
+        )}
+        <img
+          src={this.props.thumbnail_src}
+          alt="galleryItem"
+          className="uploadGalleryItemImage"
         />
-        <span
-          className="delete icon printqicon-delete"
-          onClick={() => props.deleteAsset(props.id)}
-        />
+        <div className="galleryItemActions">
+          {this.props.connectDragSource(
+            <span className="select icon printqicon-ok" />
+          )}
+          <span
+            className="delete icon printqicon-delete"
+            onClick={() => this.props.deleteAsset(this.props.id)}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 module.exports = hot(module)(
   connect(null)(DragSource(type, PageSource, collectDrag)(galleryItem))
 );
