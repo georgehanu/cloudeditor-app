63eb4abbf1a3cdfc5e657908f769effc7f6864dd
Miki <just_gabri@yahoo.com>
2019-03-18 14:01:59 +0200

    Fix fupa - matches table

--- cloudeditor/plugins/Fupa/Fupa.js
+++ cloudeditor/plugins/Fupa/Fupa.js
@@ -4,6 +4,7 @@ const assign = require("object-assign");
 const FupaBuilder = require("./FupaBuilder");
 const isEqual = require("react-fast-compare");
 const { withNamespaces } = require("react-i18next");
+const ErrorBoundary = require("../../core/components/ErrorBoundary/ErrorBoundary");
 
 class Fupa extends React.Component {
   shouldComponentUpdate = (nextProps, nextState) => {
@@ -13,7 +14,11 @@ class Fupa extends React.Component {
     return true;
   };
   render() {
-    return <FupaBuilder cfg={this.props.cfg} t={this.props.t} />;
+    return (
+      <ErrorBoundary errorMsg="Something went wrong with Fupa.">
+        <FupaBuilder cfg={this.props.cfg} t={this.props.t} />
+      </ErrorBoundary>
+    );
   }
 }
 
